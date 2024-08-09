8666ebc33784b1c2d45c47e703b72d4dfc1a70a9
Iman <imangholamiimi@gmail.com>
2024-08-09 19:59:51 +0330

    :zap: | UPDATE | used from CarManager instace and methods

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-player = Player()
 scoreboard = Scoreboard()
+player = Player()
+car = CarManager()
 
 screen.listen()
 screen.onkey(player.move, "Up")
@@ -18,4 +19,5 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
+    car.create_car()
+    car.move()
