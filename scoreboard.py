097518a1e35d1224c06e7beb0e5e6b77a024d783
Iman <imangholamiimi@gmail.com>
2024-08-09 20:36:02 +0330

    :zap: | UPDATE | added add_level method to ScoreBoard

--- scoreboard.py
+++ scoreboard.py
@@ -6,13 +6,18 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.point = 0
+        self.level = 0
         self.hideturtle()
         self.penup()
         self.color("#66CDAA")
         self.teleport(-300, 260)
-        self.write(f"Level: {self.point}", font=FONT)
+        self.write(f"Level: {self.level}", font=FONT)
 
     def game_over(self):
         self.setpos(-80, 0)
         self.write(f"Game Over", font=FONT)
+
+    def add_level(self):
+        self.clear()
+        self.level += 1
+        self.write(f"Level: {self.level}", font=FONT)
