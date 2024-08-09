6afb13ff724a84f928c09f69ae0d3599044deb8a
Iman <imangholamiimi@gmail.com>
2024-08-09 19:58:51 +0330

    :zap: | UPDATE | changed starting pos and make cars less than, to have a gap

--- car_manager.py
+++ car_manager.py
@@ -9,17 +9,18 @@ MOVE_INCREMENT = 10
 class CarManager:
     def __init__(self):
         self.cars = []
-        self.create_car()
 
     def create_car(self):
-        car = Turtle()
-        car.penup()
-        car.color(choice(COLORS))
-        car.shape("square")
-        car.setheading(180)
-        car.shapesize(stretch_wid=1, stretch_len=2)
-        car.setposition(x=270, y=randint(-250, 250))
-        self.cars.append(car)
+        random_chance = randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle()
+            new_car.penup()
+            new_car.color(choice(COLORS))
+            new_car.shape("square")
+            new_car.setheading(180)
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.setposition(x=300, y=randint(-250, 250))
+            self.cars.append(new_car)
 
     def move(self):
         for car in self.cars:
