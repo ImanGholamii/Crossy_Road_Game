15f2b0789433de7a1b076b0a947425155a31f330
Iman <imangholamiimi@gmail.com>
2024-08-10 13:46:17 +0330

    :zap: | UPDATE | added game code in play_game function to have restart functionality using from reset methods

--- main.py
+++ main.py
@@ -15,21 +15,40 @@ car_manager = CarManager()
 screen.listen()
 screen.onkey(player.move, "Up")
 
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
-    car_manager.create_car()
-    car_manager.move()
 
-    # detect collision with car
-    for car in car_manager.cars:
-        if car.distance(player) < 20:
-            scoreboard.game_over()
-            game_is_on = False
-    if player.ycor() > 280:
-        scoreboard.add_level()
-        time.sleep(0.5)
-        player.goto(0, -280)
+def play_game():
+    game_is_on = True
+    while game_is_on:
+        time.sleep(0.1)
+        screen.update()
+        car_manager.create_car()
+        car_manager.move()
 
-screen.exitonclick()
+        # detect collision with car
+        for car in car_manager.cars:
+            if car.distance(player) < 20:
+                scoreboard.game_over()
+                game_is_on = False
+        if player.ycor() > 280:
+            scoreboard.add_level()
+            time.sleep(0.5)
+            player.goto(0, -280)
+
+
+def main():
+    while True:
+        print("Starting a new game...")
+        play_game()
+        retry = screen.textinput(title="Retry?", prompt="Yes or No?")
+        if retry is None or retry.lower() != "yes":
+            break
+        scoreboard.reset()
+        player.reset()
+        car_manager.reset()
+        screen.listen()
+        screen.onkey(player.move, "Up")
+
+    screen.exitonclick()
+
+if __name__ == '__main__':
+    main()
